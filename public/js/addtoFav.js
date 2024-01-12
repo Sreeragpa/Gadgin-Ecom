9d9241194e3d4c3f791be9b3288a7101497735c3
Sreerag PA <catchsreerag@gmail.com>
2024-01-12 11:29:03 +0530

    Fixed Cartpage ejs syntax error

--- public/js/addtoFav.js
+++ public/js/addtoFav.js
@@ -1,26 +1,26 @@
-document.querySelectorAll(".add-wishlist").forEach((element)=>{
-    element.addEventListener('click',(e)=>{
+document.querySelectorAll(".add-wishlist").forEach((element) => {
+    element.addEventListener('click', (e) => {
         e.preventDefault()
         const href = element.getAttribute('href');
         $.ajax({
-            url:href,
-            method:"PUT",
+            url: href,
+            method: "PUT",
             success: function (data) {
-       const samepage = $(data).find('#mywishlist').html();
+                const samepage = $(data).find('#mywishlist').html();
 
-       if(samepage){
-        const finder = element.getAttribute('id');
-        console.log(finder);
-        const newelement = $(data).find(`#${finder}`);
-        console.log(newelement);
-        $(element).html(newelement.html());
-       }else{
-        $('body').html(data)
-       }
-    },
-    error: function () {
-        window.location.href = '/login'; 
-    }
+                if (samepage) {
+                    const finder = element.getAttribute('id');
+                    console.log(finder);
+                    const newelement = $(data).find(`#${finder}`);
+                    console.log(newelement);
+                    $(element).html(newelement.html());
+                } else {
+                    $('body').html(data)
+                }
+            },
+            error: function () {
+                window.location.href = '/login';
+            }
         })
     })
 })
